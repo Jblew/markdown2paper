7e0ba3846be1b849047315d41ade261701fcff7b
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-02-04 18:31:45 +0100

    chore: make single level mod structure

--- demo/build-n-test.sh
+++ demo/build-n-test.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}"
 set -e
 
-SRC_DIR="../markdown2paper"
+SRC_DIR=".."
 BUILD_CMD="${SRC_DIR}/build.sh"
 RUN_CMD="${SRC_DIR}/markdown2paper"
 
